10dff43b12f20cccc7766404fa0298c850da32a9
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-09-23 12:28:46 +1000

    chore: set owner & rewards as the same in deploy

--- ignition/modules/StakingRewards.ts
+++ ignition/modules/StakingRewards.ts
@@ -1,6 +1,5 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 
-export const REWARDS_DISTRIBUTION = '0xEc84524385ffD1eA423Ec4682caa5AB56071FbC7';
 export const REWARDS_TOKEN = '0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d'; // FOX
 export const STAKING_TOKEN = '0x470e8de2eBaef52014A47Cb5E6aF86884947F08c'; // ETH/FOX Pool
 export const OWNER = '0x90A48D5CF7343B08dA12E067680B4C6dbfE551Be'; // DAO multi-sig
@@ -8,7 +7,7 @@ export const OWNER = '0x90A48D5CF7343B08dA12E067680B4C6dbfE551Be'; // DAO multi-
 const StakingRewardsModule = buildModule('StakingRewardsModule', (m) => {
   const deployer = m.getAccount(0);
   const owner = m.getParameter('owner', OWNER);
-  const rewardsDistribution = m.getParameter('rewardsDistribution', REWARDS_DISTRIBUTION);
+  const rewardsDistribution = m.getParameter('rewardsDistribution', OWNER);
   const rewardsToken = m.getParameter('rewardsToken', REWARDS_TOKEN);
   const stakingToken = m.getParameter('stakingToken', STAKING_TOKEN);
 
